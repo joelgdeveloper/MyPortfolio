00e95d112d5c869fae10f89682f30b8cb6922719
Joel <joelgdeveloper@gmail.com>
2022-08-04 11:41:37 -0300

    componente work modificado

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
+import { AiOutlineEye } from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 const Work = () => {
   return (
@@ -14,7 +16,7 @@ const Work = () => {
         </div>
 
         {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 '>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
             {/* Grid Item */}
           <div
@@ -22,24 +24,21 @@ const Work = () => {
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className='opacity-0 group-hover:opacity-100 group-hover:duration-700'>
+              <span className='text-2xl font-bold text-[#0A192F] tracking-wider'>
                 React JS Application
               </span>
-              <div className='pt-8 text-center'>
+              <div className='pt-8 text-center flex justify-center items-center gap-5'>
                 <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
+                  <AiOutlineEye size={50} className="text-[#0A192F] hover:text-[#E31B6D] duration-300" />
                 </a>
                 <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
+                  <AiOutlineGithub size={42} className="text-[#0A192F] hover:text-[#E31B6D] duration-300" />
                 </a>
               </div>
             </div>
           </div>
+          
           <div
             style={{ backgroundImage: `url(${realEstate})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
