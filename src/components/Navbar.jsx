954eb7ccaadc449fd0613b783f4ed85cf8228294
Joel <joelgdeveloper@gmail.com>
2022-08-04 10:30:11 -0300

    Skills - Arreglando bug en skills

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-10'>
       <div>
         <Link to="home" smooth={true} duration={500} className='cursor-pointer'>
           <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -23,52 +23,52 @@ const Skills = () => {
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Html} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Html} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>HTML</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Css} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Css} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>CSS</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Javascript} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group '>
+            <img src={Javascript} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>JavaScript</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={ReactImg} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={ReactImg} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>React</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Sass} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Sass} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Sass</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Tailwind} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Tailwind} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Tailwind CSS</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Node} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Node} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Node JS</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Express} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Express} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Express JS</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Mongo} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Mongo} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Mongo DB</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Npm} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Npm} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>NPM</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Git} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Git} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Git</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Heroku} alt="HTML Icon" className='w-20 mx-auto' />
+          <div className='shadow-md shadow-[#040c16] group'>
+            <img src={Heroku} alt="HTML Icon" className='w-20 mx-auto group-hover:scale-110 duration-500' />
             <p className='my-4'>Heroku</p>
           </div>
         </div>
